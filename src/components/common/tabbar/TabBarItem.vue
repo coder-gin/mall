f4f32c2a88ca50d186972e9c2c51b059e9743f16
Gin <215129113@qq.com>
2020-08-22 13:31:15 +0800

    completed tabbar

---
+++ src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="tab-bar-item" @click="itemClick">
+    <div v-if="!isActive">
+      <slot name="item-icon"></slot>
+    </div>
+    <div v-else>
+      <slot name="item-icon-active"></slot>
+    </div>
+    <div :style="activeStyle">
+      <slot name="item-text"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TarBarItem',
+  props: {
+    path: String,
+    activeColor: {
+      type: String,
+      default: '#ff5777'
+    }
+  },
+  computed: {
+    // 根据这个路由的路径（this.$route.path）与this.path的路径是否一致来判断当前是否处于活跃
+    isActive () {
+      return this.$route.path.includes(this.path)
+    },
+    // 判断当前是否活跃动态改变文字颜色
+    activeStyle () {
+      return this.isActive ? { color: this.activeColor } : {}
+    }
+  },
+  methods: {
+    itemClick () {
+      this.$router.push(this.path)
+    }
+  },
+}
+</script>
+
+<style>
+.tab-bar-item {
+  flex: 1;
+  text-align: center;
+  height: 49px;
+}
+
+.tab-bar-item img {
+  width: 24px;
+  height: 24px;
+}
+</style>
