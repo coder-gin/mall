cfb9cfb2206876bfcb2a6f407136068bdc98614a
Gin <215129113@qq.com>
2020-08-28 17:22:30 +0800

    completed tabbar

--- src/components/common/tabbar/TabBarItem.vue
+++ src/components/common/tabbar/TabBarItem.vue
@@ -11,44 +11,42 @@
     </div>
   </div>
 </template>
-
 <script>
-export default {
-  name: 'TarBarItem',
-  props: {
-    path: String,
-    activeColor: {
-      type: String,
-      default: '#ff5777'
-    }
-  },
-  computed: {
-    // 这个路由的路径（this.$route.path）与this.path的路径是否一致来判断当前是否处于活跃
-    isActive () {
-      return this.$route.path.includes(this.path)
+  export default {
+    name: 'TarBarItem',
+    props: {
+      path: String,
+      activeColor: {
+        type: String,
+        default: '#ff5777'
+      }
     },
-    // 判断当前是否活跃动态改变文字颜色
-    activeStyle () {
-      return this.isActive ? { color: this.activeColor } : {}
-    }
-  },
-  methods: {
-    itemClick () {
-      this.$router.push(this.path)
-    }
-  },
-}
+    computed: {
+      // 这个路由的路径（this.$route.path）与this.path的路径是否一致来判断当前是否处于活跃
+      isActive() {
+        return this.$route.path.includes(this.path)
+      },
+      // 判断当前是否活跃动态改变文字颜色
+      activeStyle() {
+        return this.isActive ? { color: this.activeColor } : {}
+      }
+    },
+    methods: {
+      itemClick() {
+        this.$router.push(this.path)
+      }
+    },
+  }
 </script>
+<style scoped>
+  .tab-bar-item {
+    flex: 1;
+    text-align: center;
+    height: 49px;
+  }
 
-<style>
-.tab-bar-item {
-  flex: 1;
-  text-align: center;
-  height: 49px;
-}
-
-.tab-bar-item img {
-  width: 24px;
-  height: 24px;
-}
+  .tab-bar-item img {
+    width: 24px;
+    height: 24px;
+  }
 </style>
